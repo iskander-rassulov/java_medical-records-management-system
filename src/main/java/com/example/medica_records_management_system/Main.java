afcf5f38c4b4db863269ab07de5b2668dded054d
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-09-19 15:54:03 +0500

    Main

--- src/main/java/com/example/medica_records_management_system/Main.java
+++ src/main/java/com/example/medica_records_management_system/Main.java
@@ -1,4 +1,35 @@
 package com.example.medica_records_management_system;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Загрузка FXML файла
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view_main.fxml")));
+
+            // Установка сцены
+            Scene scene = new Scene(root);
+
+            // Установка заголовка и сцены
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("Medical Records Management System");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
