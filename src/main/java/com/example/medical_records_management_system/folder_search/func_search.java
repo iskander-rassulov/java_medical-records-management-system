34d51b23c99d919b152d5277a2531978cd309ffb
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-09-22 17:52:57 +0500

    search records fxml

--- src/main/java/com/example/medical_records_management_system/folder_search/func_search.java
+++ src/main/java/com/example/medical_records_management_system/folder_search/func_search.java
@@ -1,4 +1,13 @@
 package com.example.medical_records_management_system.folder_search;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
 public class func_search {
+    public JFXButton button_click_search;
+    public ScrollPane search_scroll_pane;
+    public TextField search_bar;
+    public ChoiceBox choice_box;
 }
