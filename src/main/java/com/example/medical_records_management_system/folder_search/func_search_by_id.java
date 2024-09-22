2e2e9e83fe88b00877bfbb634b7ebdfceda62f29
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-09-22 12:54:38 +0500

    database update + search setup

---
+++ src/main/java/com/example/medical_records_management_system/folder_search/func_search_by_id.java
@@ -0,0 +1,4 @@
+package com.example.medical_records_management_system.folder_search;
+
+public class func_search_by_id {
+}
---
+++ src/main/java/com/example/medical_records_management_system/folder_search/func_table_of_records.java
@@ -0,0 +1,4 @@
+package com.example.medical_records_management_system.folder_search;
+
+public class func_table_of_records {
+}
