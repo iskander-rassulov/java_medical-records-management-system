eae2ba6418ba973b6a32bb2444aec6ff694eeab7
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-09-26 14:23:55 +0500

    patient information added

--- src/main/java/com/example/medical_records_management_system/folder_patient/func_open_patient_info.java
+++ src/main/java/com/example/medical_records_management_system/folder_patient/func_open_patient_info.java
@@ -9,13 +9,15 @@ import java.io.IOException;
 public class func_open_patient_info {
 
     public void showPatientInfo(AnchorPane right_pane, data_patients patient) {
-        if (right_pane == null) {
-            System.out.println("Error: right_pane is null в func_open_patient_info");
-            return;
-        }
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/medical_records_management_system/view_patient_info.fxml"));
             AnchorPane patientInfoView = loader.load();
+
+            // Получаем контроллер
+            func_patient_info patientInfoController = loader.getController();
+
+            // Передаем данные пациента
+            patientInfoController.setPatientInfo(patient);
 
             right_pane.getChildren().clear();
             right_pane.getChildren().add(patientInfoView);
--- src/main/java/com/example/medical_records_management_system/folder_patient/func_patient_info.java
+++ src/main/java/com/example/medical_records_management_system/folder_patient/func_patient_info.java
@@ -1,17 +1,47 @@
 package com.example.medical_records_management_system.folder_patient;
 
+import com.example.medical_records_management_system.data_doctor;
+import com.example.medical_records_management_system.data_patients;
 import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class func_patient_info {
+    @FXML
     public Text valName;
+    @FXML
     public Text valSurname;
+    @FXML
     public Text valDateOfBirth;
+    @FXML
     public Text valGender;
+    @FXML
     public Text valPhoneNumber;
+    @FXML
     public Text valEmail;
+    @FXML
     public Text valAddress;
+    @FXML
     public Text valRecordId;
+    @FXML
     public JFXButton button_patient_manage_patient_info;
+    @FXML
     public JFXButton button_patient_open_medical_record;
+
+    // Метод для установки данных о докторе
+    public void setPatientInfo(data_patients patient) {
+        valName.setText(patient.getFirstName()+" ");
+        valSurname.setText(patient.getSecondName());
+        valDateOfBirth.setText(patient.getDateOfBirth());
+        valGender.setText(patient.getGender());
+        valPhoneNumber.setText(patient.getPhoneNumber());
+        valEmail.setText(patient.getEmail());
+        valAddress.setText(patient.getAddress());
+        valRecordId.setText(String.valueOf(patient.getMedicalRecordId()));
+
+        // Если необходимо, добавьте остальные поля (например, дату рождения, пол и др.)
+        // valDateOfBirth.setText(...); // Пример использования
+        // valGender.setText(...); // Пример использования
+    }
+
 }
