39a92cdc97ed4e579291a6a57051f0a65b7e1854
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-09-29 02:15:33 +0500

    view_make_record.fxml designed

--- src/main/java/com/example/medical_records_management_system/folder_make_record/func_make_record.java
+++ src/main/java/com/example/medical_records_management_system/folder_make_record/func_make_record.java
@@ -1,4 +1,13 @@
 package com.example.medical_records_management_system.folder_make_record;
 
+import javafx.scene.control.TextField;
+
 public class func_make_record {
+    public TextField valFirstName;
+    public TextField valDateOfBirth;
+    public TextField valSecondName;
+    public TextField valGender;
+    public TextField valPhoneNumber;
+    public TextField valEmail;
+    public TextField valAddress;
 }
